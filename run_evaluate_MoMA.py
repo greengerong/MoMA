385480e6206fa522d1bd4eba82a283b421caa6d0
green <grzx2210@163.com>
2024-05-21 21:06:52 +0800

    Update run_evaluate_MoMA.py

--- run_evaluate_MoMA.py
+++ run_evaluate_MoMA.py
@@ -33,23 +33,26 @@ moMA_main_modal = MoMA_main_modal(args).to(args.device, dtype=args.dtype)
 
 
 # reference image and its mask
-rgb_path = "example_images/newImages/3.jpg"
-mask_path = "example_images/newImages/3_mask.jpg"
-subject = 'car'
+# rgb_path = "example_images/newImages/3.jpg"
+# mask_path = "example_images/newImages/3_mask.jpg"
+# subject = 'car'
 
 
-# Let's generate new images!
+# # Let's generate new images!
 
-################ change context ##################
-prompt = "A car in autumn with falling leaves."
-generated_image = moMA_main_modal.generate_images(rgb_path, mask_path, subject, prompt, strength=1.0, seed=2, return_mask=True)  # set strength to 1.0 for more accurate details
-save_image(generated_image,f"{args.output_path}/{subject}_{prompt}.jpg")
+# ################ change context ##################
+# prompt = "A car in autumn with falling leaves."
+# generated_image = moMA_main_modal.generate_images(rgb_path, mask_path, subject, prompt, strength=1.0, seed=2, return_mask=True)  # set strength to 1.0 for more accurate details
+# save_image(generated_image,f"{args.output_path}/{subject}_{prompt}.jpg")
 
-################ change texture ##################
-prompt = "A wooden sculpture of a car on the table."
-generated_image = moMA_main_modal.generate_images(rgb_path, mask_path, subject, prompt, strength=0.4, seed=4, return_mask=True)  # set strength to 0.4 for better prompt fidelity
-save_image(generated_image,f"{args.output_path}/{subject}_{prompt}.jpg")
+# ################ change texture ##################
+# prompt = "A wooden sculpture of a car on the table."
+# generated_image = moMA_main_modal.generate_images(rgb_path, mask_path, subject, prompt, strength=0.4, seed=4, return_mask=True)  # set strength to 0.4 for better prompt fidelity
+# save_image(generated_image,f"{args.output_path}/{subject}_{prompt}.jpg")
 
+def run_moma(subject:str, prompt:str,rgb_path:str, mask_path:str):
+  generated_image = moMA_main_modal.generate_images(rgb_path, mask_path, subject, prompt, strength=1.0, seed=2, return_mask=True)  # set strength to 1.0 for more accurate details
+  return generated_image
 
 
 
